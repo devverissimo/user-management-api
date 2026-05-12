1c0f24112d7b5f87e785bd126f89495e8bf30caf
Maria <dev.verissimo@gmail.com>
2026-05-12 10:43:55 -0300

    feat: add UsuarioRepository

---
+++ src/main/java/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import com.maria.user_management_api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByUsername(String username);
+}
